1a7dd6f21d4d62241ef0956cd3ad9b5b4fa423d3
Ryo Toyota <t1910446@edu.cc.uec.ac.jp>
2020-03-13 21:32:16 +0900

    add sieve code

--- libs/prime.rb
+++ libs/prime.rb
@@ -12,3 +12,25 @@ def isPrime(n)
 	
 	return true
 end
+
+def sieve(n)
+	primes = []
+	a = Array.new(n, true)
+	2.step(Math.sqrt(n).to_i) do |i|
+		if a[i]
+			primes.push(i)
+			
+			i.step(n-1, i) do |k|
+				a[k] = false
+			end
+		end
+	end
+
+	Math.sqrt(n).to_i.step(n-1) do |i|
+		if a[i]
+			primes.push(i)
+		end
+	end
+
+	return primes
+end
