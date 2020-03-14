9dff696465ac029e6e11c1a012389fd01d63c9f9
Ryo Toyota <t1910446@edu.cc.uec.ac.jp>
2020-03-14 21:06:32 +0900

    solve completely

--- p031.rb
+++ p031.rb
@@ -1,3 +1,22 @@
+# reffered: https://qiita.com/cof/items/1a38e1303d3a7d84d05e
+
 def main
-	
+	coins = [1, 2, 5, 10, 20, 50, 100, 200]
+	# coins = [2, 5, 10, 20, 50, 100, 200]
+	return count(coins, 200)
+end
+
+def count(coins, target)
+	if coins.length == 1
+		return 1
+	else
+		count = 0
+		coin = coins.pop
+		cycle = (target / coin) + 1
+		cycle.times do |i|
+			count += count(coins.dup, target - coin * i)
+		end
+
+		return count
+	end
 end
