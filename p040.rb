01ad5d713d2997406cddb384f355f4e0d576d28b
Ryo Toyota <t1910446@edu.cc.uec.ac.jp>
2020-03-15 00:49:47 +0900

    solve completely

--- p040.rb
+++ p040.rb
@@ -1,3 +1,24 @@
+$d = []
+
 def main
-	
+	gen
+	x = 1
+	7.times do |i|
+		x *= $d[10**i - 1]
+	end
+
+	return x
+end
+
+def gen
+	# a = []
+	n = 1
+	while $d.length < 10**6 + 100
+		s = n.to_s
+		s.length.times do |i|
+			$d.push(s[i].to_i)
+		end
+
+		n += 1
+	end
 end
