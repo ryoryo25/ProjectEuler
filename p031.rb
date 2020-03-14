71dd69c3b17e244d167bdf889f31fc15eabebf9b
Ryo Toyota <t1910446@edu.cc.uec.ac.jp>
2020-03-14 23:04:53 +0900

    add comment

--- p031.rb
+++ p031.rb
@@ -2,20 +2,19 @@
 
 def main
 	coins = [1, 2, 5, 10, 20, 50, 100, 200]
-	# coins = [2, 5, 10, 20, 50, 100, 200]
 	return count(coins, 200)
 end
 
 def count(coins, target)
-	if target == 0 # if the recursion reaches this block, 
+	if target == 0 # if the recursion reaches this block, 200p was built
 		return 1
-	elsif coins.length == 1 # if the recursion reaches this block, 
+	elsif coins.length == 1 # if the recursion reaches this block, all the rest can be filled with 1p coin, so always return 1
 		return 1
 	else
 		count = 0
 		coin = coins.pop
 		cycle = (target / coin) + 1 # to include not using the coin, add 1
-		
+
 		cycle.times do |i|
 			count += count(coins.dup, target - coin * i)
 		end
