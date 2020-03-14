ab0d49af30f5c399dc4c747e3d1eaa1d8866d26b
Ryo Toyota <t1910446@edu.cc.uec.ac.jp>
2020-03-15 00:36:56 +0900

    solve completely

--- p041.rb
+++ p041.rb
@@ -1,11 +1,52 @@
 require './libs/prime.rb'
 
 def main
-	primes = sieve(1000000000-1)
-
-
+	9.step(1, -1) do |i|
+		perm(i).each do |x|
+			if isPrime(x)
+				return x
+			end
+		end
+	end
 end
 
-def isPandigital(x, n = x.to_s.length)
+def perm(n)
+	a = Array.new(n) {|i| i+1 }
+	perms = a.permutation.to_a
+	ans = []
 
+	perms.each do |x|
+		s = ""
+		x.each do |i|
+			s += i.to_s
+		end
+		ans.push(s.to_i)
+	end
+
+	return ans.reverse
+end
+
+# unused
+def isPandigital(x)
+	s = x.to_s.split(//)
+	n = s.length
+	digits = []
+	check = Array.new(n) {|i| i+1 }
+
+	n.times do |i|
+		k = s[i].to_i
+		if !digits.include?(k)
+			digits.push(k)
+		else
+			return false
+		end
+	end
+
+	digits.sort!
+	flg = (digits.length == n)
+	n.times do |i|
+		flg = flg && (digits[i] == check[i])
+	end
+
+	return flg
 end
