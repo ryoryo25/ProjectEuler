be7d701ccc944c1ccea3d236904dea9e20d84946
Ryo Toyota <t1910446@edu.cc.uec.ac.jp>
2020-03-15 18:51:40 +0900

    add giving block code

--- libs/read.rb
+++ libs/read.rb
@@ -6,3 +6,18 @@ def readFile(path)
 
 	return a
 end
+
+def readLines(path)
+	a = []
+	File.open(path) do |f|
+		f.each_line do |line|
+			if block_given?
+				a.push(yield(line))
+			else
+				a.push(line.gsub!(/"/, '').gsub!(/,/, ' ').split.sort)
+			end
+		end
+	end
+
+	return a
+end
