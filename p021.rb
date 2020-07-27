656e6a40140c5645ed75961e7b3d59c86ad2a360
ry_Tom <t1910446@edu.cc.uec.ac.jp>
2020-07-28 00:25:11 +0900

    solve completely

--- p021.rb
+++ p021.rb
@@ -1,11 +1,18 @@
+$amicable = [0]
+
+# Reference
+# https://www.mathblog.dk/project-euler-21-sum-of-amicable-pairs/
+
 def main
+	upper = 10000
 	sum = 0
 	amicables = []
 
-	10000.times do |a|
-		10000.times do |b|
-			if isAmicable(a, b) && !(amicables.include?([a, b]) || amicables.include?([a, b]))
-				amicables.push([a, b])
+	2.step(upper) do |a|
+		b = d(a) # sum_a = b
+		if b > a && b <= upper # b < a: the b has already checked, a == b: not allowed
+			sum_b = d(b)
+			if sum_b == a
 				sum += a + b
 			end
 		end
@@ -14,21 +21,18 @@ def main
 	puts(sum)
 end
 
-def isAmicable(a, b)
-	if a == b
-		return false
-	else
-		return d(a) == d(b)
-	end
-end
-
 def d(n)
+	if $amicable[n] != nil
+		return $amicable[n]
+	end
+
 	sum = 0
 	(n-1).times do |i|
 		if n % (i+1) == 0
 			sum += i+1
 		end
 	end
+	$amicable[n] = sum
 
 	return sum
 end
