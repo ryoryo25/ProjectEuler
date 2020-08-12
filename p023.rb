cf96ce3652e41f95d867f6cb87a7ffa14d9e6d73
ry_Tom <t1910446@edu.cc.uec.ac.jp>
2020-08-12 14:20:58 +0900

    solve completely

--- p023.rb
+++ p023.rb
@@ -1,26 +1,58 @@
+# Reference
+# https://tasusu.hatenablog.com/entry/20110819/1313770567
+
 def main
+	max = 28123
 	abundants = []
-	28123.times do |i|
+	1.step(max) do |i|
 		if isAbundant(i+1)
 			abundants.push(i+1)
 		end
 	end
 
-	sum = 0
-	28123.times do |i|
-		
+	p abundants
+
+	cache = Array.new(max+1, false)
+	abundants.each do |x|
+		cache[x] = true
 	end
+
+	sum = 395465626 # Sum[0,28123](n) = 395465626
+	1.step(max) do |i|
+	# 1.step(100) do |i|
+		if canBeSum(i, abundants, cache)
+			sum -= i
+		end
+	end
+	
+	puts sum
+
 end
 
-def canBeSum(n, a)
+# abundants[i] + abundants[j] = n
+# abundants[j] = n - abundants[i] > 0
+def canBeSum(n, array, cache)
+	i = 0
+	while array[i] <= n / 2
+		if cache[n-array[i]] # array.include?(n-array[i]) is OK, but very slow
+			return true
+		end
 
+		i += 1
+	end
+
+	return false
 end
 
 def isAbundant(n)
 	sum = 0
-	(n-1).times do |i|
-		if n % (i+1) == 0
-			sum += i+1
+	2.step(Math.sqrt(n).to_i) do |i|
+		if n % i == 0
+			sum += i
+			if n / i != i # n / i == i means n is square number
+				sum += n/i # n = i * (n / i)
+			end
+		end
 	end
 
 	return sum > n
