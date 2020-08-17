93f1050b639212f81672832c7ad79e2054310288
ry_Tom <t1910446@edu.cc.uec.ac.jp>
2020-08-18 02:27:33 +0900

    refactoring isPrime and add function to find prime factors and all factors

--- libs/prime.rb
+++ libs/prime.rb
@@ -2,9 +2,16 @@ def isPrime(n)
 	if n <= 1
 		return false
 	end
+
+	if n == 2
+		return true
+	end
+	if n % 2 == 0
+		return false
+	end
 	
 	up = Math.sqrt(n).to_i
-	(2..up).each do |i|
+	3.step(up, 2) do |i|
 		if n % i == 0
 			return false
 		end
@@ -33,4 +40,53 @@ def sieve(n)
 	end
 
 	return primes
+end
+
+def primeFactors(n)
+	factors = []
+	factor = 3
+
+	if n % 2 == 0
+		n /= 2
+		factors.push(2)
+		while n % 2 == 0
+			n /= 2
+		end
+	end
+
+	maxFactor = Math.sqrt(n)
+	while n > 1 && factor <= maxFactor
+		if n % factor == 0
+			n /= factor
+			factors.push(factor)
+			while n % factor == 0
+				n /= factor
+			end
+			maxFactor = Math.sqrt(n) # update max
+		end
+
+		factor += 2
+	end
+
+	if n != 1
+		factors.push(n)
+	end
+
+	return factors
+end
+
+# 1 ... n
+def getAllFactors(n)
+	factors = [1]
+	2.step(Math.sqrt(n).to_i) do |i|
+		if n % i == 0
+			factors.push(i)
+			if n / i != i
+				factors.push(n/i)
+			end
+		end
+	end
+	factors.push(n)
+
+	return factors.sort
 end
